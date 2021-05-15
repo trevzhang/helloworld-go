b45953245e52ee47ef8bd723537b0c2c1a8d4334
trevzhang <trevzhang@gmail.com>
2021-05-15 11:19:15 +0800

    add echo2.go

---
+++ main/echo2.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	str, separate := "",""
+	for _,arg := range os.Args[1:] {
+		str += separate + arg
+		separate = " "
+	}
+	fmt.Println(str)
+}
