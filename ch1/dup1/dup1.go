ad549ec8cba052ce1c473fb172bd9d2bf291da8d
trevzhang <trevzhang@gmail.com>
2021-05-15 14:30:00 +0800

    优化dup1代码

--- ch1/dup1/dup1.go
+++ ch1/dup1/dup1.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	counts := make(map[string]int)
 	input := bufio.NewScanner(os.Stdin)
+	fmt.Println("统计输入重复的字符串")
+	fmt.Println("请输入字符串(输入quit停止继续输入):")
 	for input.Scan() {
 		var t string
 		if t = input.Text(); t == "quit" {
@@ -17,9 +19,10 @@ func main() {
 		counts[t]++
 	}
 	// 注意:忽略input.Err()中可能的错误
+	fmt.Printf("字符串\t出现次数\n")
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+			fmt.Printf("%s\t%d\n", line, n)
 		}
 	}
 }
