904d24b42f816f85a5ca3299fa9fceec2c162149
trevzhang <trevzhang@gmail.com>
2021-05-15 11:47:26 +0800

    add dup1.go

---
+++ main/dup1.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		var t string
+		if t = input.Text(); t == "quit"{
+			break
+		}
+		counts[t]++
+	}
+	// 注意:忽略input.Err()中可能的错误
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
