fc56f4b9c1cb962851d61b6c3f2d63fc0864e129
trevzhang <trevzhang@163.com>
2021-07-05 19:10:11 +0800

    测试指针

---
+++ test/testSlicePointer.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	test2()
+}
+
+// 切片是指针类型，修改切片会修改数组
+func test1() {
+	a1 := [3]int{1, 2, 3}
+	fmt.Println("array: \t\t", a1)
+	s1 := a1[1:3]
+	fmt.Println("slice: \t\t", s1)
+	s1[0] = 20
+	s1[1] = 30
+	fmt.Println("modified array: ", a1)
+}
+
+// 切片扩容后修改切片，不会影响原有数组
+func test2() {
+	a1 := [3]int{1, 2, 3}
+	fmt.Println("array: \t\t", a1)
+	s1 := a1[1:3]
+	fmt.Println("slice: \t\t", s1)
+	s1 = append(s1, 4, 5, 6)
+	s1[0] = 20
+	s1[1] = 30
+	fmt.Println("modified slice: ", s1)
+	fmt.Println("modified array: ", a1)
+}
